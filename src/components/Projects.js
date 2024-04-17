bcf0276dfb9565e1dbfa6ca418ac500dc3c3cb39
pradeep L <84018027+Pradeeplaskshmanan@users.noreply.github.com>
2024-04-17 20:56:33 +0530

    Update Projects.js
    image description

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import Anemiaimg from '../assets/Anemia.png'
 import DeepFake from '../assets/DeepFake Detection .png'
-import Website from '../assets/food-ecommerce.jpg'
+
 export default function Projects(){
     return <section id='projects' className="flex flex-col py-20 px-5 justify-center bg-primary text-white">
         <div className="w-1/2">
@@ -13,14 +13,14 @@ export default function Projects(){
         <div className="w-full">
            <div className='flex flex-col md:flex-row px-10 gap-10'>
             <div className='relative'>
-                <img className='h-[300px]' src={Anemiaimg}/> 
+                <img className='h-[300px]' src={Anemiaimg} alt=""/> 
                 <div>
                     <p className='flex text-3xl'>Virtual Hue: Anemia Prognosis</p>
                     <a href='https://anemia-prediction-web-applicaition-ipablanqswbrkkkzxykrry.streamlit.app/'><button className='btn1'>Live Now</button></a>
                 </div>  
             </div>
             <div>
-                <img className='h-[300px]' src={DeepFake}/>
+                <img className='h-[300px]' src={DeepFake} alt=""/>
                 <div>
                     <p className='flex text-3xl'>Deep Fake Detection</p>
                     <a href='https://github.com/Pradeeplaskshmanan/Deep-Fake-Detection'><button className='btn1'>Repo Link</button></a>
