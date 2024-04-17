7c4142c851d2961b04c02ec646db088d52f5d38d
pradeep L <84018027+Pradeeplaskshmanan@users.noreply.github.com>
2024-04-17 20:52:35 +0530

    Update Header.js
    changed the href with /

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,12 @@ import { useState } from 'react'
 export default function Header(){
     const [toggleMenu,setToggleMenu]= useState(false);
     return <header className="flex justify-between px-5 py-2 bg-primary">
-        <a className="font-bold text-black" href='#'>pradeep L</a>
+        <a className="font-bold text-black" href='/'>pradeep L</a>
         <nav className="hidden md:block">
         <ul className="flex text-white">
             <li><a href='/'>Home</a></li>
             <li><a href='#about'>About</a></li>
-            <li><a href='#projects'>projects</a></li>
+            <li><a href='#projects'>Projects</a></li>
             <li><a href='#Resume'>Resume</a></li>
             <li><a href='#contact'>Contacts</a></li>
         </ul>
